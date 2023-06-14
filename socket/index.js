207fc021bd47cc7e3be574b6d9e781ab5678617f
NehaS101 <solankineha297@gmail.com>
2023-06-14 22:36:59 +0530

    added

--- socket/index.js
+++ socket/index.js
@@ -6,7 +6,9 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-
+io.on('connection',(socket)=>{
+    console.log('client connected'+socket.id);
+})
 
 server.listen(3500,()=>{
     console.log("client connected on port 3500")
