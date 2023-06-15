8e05040625657d0d39a6287e271677f369b3ed70
NehaS101 <solankineha297@gmail.com>
2023-06-15 14:46:21 +0530

    added

--- socket/index.js
+++ socket/index.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const app = express();
 const http = require('http');
 const socketIo = require('socket.io');
 
-const app = express();
+
 const server = http.createServer(app);
 const io = socketIo(server);
 
 io.on('connection',(socket)=>{
-    console.log('client connected'+socket.id);
+    console.log('client connected '+socket.id);
+
+    const targetClientID = "Lee8GqDjnosdhrjhAAAD";
+    io.to(targetClientID).emit('message',"hello world");
+    
+    socket.on('disconnect', () => {
+        console.log('A client disconnected: ' + socket.id);
+      });
 })
 
-server.listen(3500,()=>{
-    console.log("client connected on port 3500")
+
+server.listen(3000,()=>{
+    console.log("client connected on port 3000")
 })
