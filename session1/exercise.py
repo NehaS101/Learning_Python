1242d0bed684dcbbcb93bce54486827099fa3f68
NehaS101 <solankineha297@gmail.com>
2023-07-03 22:32:45 +0530

    doing session1 exercise

---
+++ session1/exercise.py
@@ -0,0 +1,9 @@
+'''
+Write a program that prompts the user for their name, age, and favorite color.
+Then, display the collected information in the following format:
+
+Your name is {name}, you are {age} years old, and your favorite color is {color}.
+
+Make sure to handle any potential errors, such as invalid input or empty input.
+'''
+
