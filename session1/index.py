b1154b2056c71a79c60843f2b4c588d2c9184b0d
NehaS101 <solankineha297@gmail.com>
2023-07-03 22:02:13 +0530

    variable and print statement

---
+++ session1/index.py
@@ -0,0 +1,10 @@
+#variables and data types
+name="neha solanki"
+age=19
+score=92.8
+student=True
+
+#print statements
+print("my name is",name,"and age is",age)
+print(f"my name is {name}. I'm {age} years old and I scored {score}% in 12th")
+
