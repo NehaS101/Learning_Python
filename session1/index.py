21421c690f04c1b8ba9d9fec51973c8645c007a2
NehaS101 <solankineha297@gmail.com>
2023-07-03 22:20:57 +0530

    string manipulation

--- session1/index.py
+++ session1/index.py
@@ -8,3 +8,18 @@ student=True
 print("my name is",name,"and age is",age)
 print(f"my name is {name}. I'm {age} years old and I scored {score}% in 12th")
 
+#string manipulation
+mssg="hello"
+print(mssg[1])
+mssg2="world"
+
+#sting concatenation
+full_mssg = mssg+" "+mssg2
+
+print(full_mssg)
+
+#string methods
+news="   World of the future  "
+print(news.upper())
+print(news.lower())
+print(news.strip())
