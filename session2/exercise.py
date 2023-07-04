c514fb2d355628c8051d39ef07e1448f43a6fe73
NehaS101 <solankineha297@gmail.com>
2023-07-04 17:30:24 +0530

    session2 exercise start

---
+++ session2/exercise.py
@@ -0,0 +1,15 @@
+'''
+Exercise 1:
+Write a program that checks if a number is prime. 
+Prompt the user to enter a number, and then display whether it is prime or not. 
+A prime number is a number greater than 1 that has no divisors other than 1 and itself.
+'''
+
+
+
+'''
+Exercise 2:
+Write a program that calculates the factorial of a given number. 
+Prompt the user to enter a positive integer, and then display its factorial. 
+The factorial of a number is the product of all positive integers less than or equal to that number.
+'''
