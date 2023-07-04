85cf4dcd7b4e41d6feb484bceb1788f1c85a950c
NehaS101 <solankineha297@gmail.com>
2023-07-04 14:39:29 +0530

    conditional statements

--- session2/index.py
+++ session2/index.py
@@ -3,3 +3,10 @@ conditional statements
 if,elif(else-if),else
 '''
 
+temp=9
+if temp>30:
+    print("it's too hot")
+elif temp<15:
+    print("its cold")
+else:
+    print("moderate temperature")        
