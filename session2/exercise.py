4c2267546de45c0df04bbb940daeab054439057f
NehaS101 <solankineha297@gmail.com>
2023-07-04 20:58:05 +0530

    exercise1 done

--- session2/exercise.py
+++ session2/exercise.py
@@ -4,7 +4,22 @@ Write a program that checks if a number is prime.
 Prompt the user to enter a number, and then display whether it is prime or not. 
 A prime number is a number greater than 1 that has no divisors other than 1 and itself.
 '''
+import math
+def check(num):
+    if num<2:
+        return False
+    prime=True
+    for i in range(2,int(math.sqrt(num))+1) :
+        if num%i==0:
+            prime=False
+            break
+    return prime
 
+output=check(5)
+if output==True:
+    print("Number is prime")
+else:
+    print("Number is not prime")
 
 
 '''
