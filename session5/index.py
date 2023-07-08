e62ef7fdbd8780444cabbab350ba98390370b962
NehaS101 <solankineha297@gmail.com>
2023-07-08 22:54:45 +0530

    encapsulation done

--- session5/index.py
+++ session5/index.py
@@ -1,6 +1,8 @@
 #OOP(object oriented programming)
 
 #classes
+
+#class definition
 class Person:
     def __init__(self,name,age):
         self.name = name
@@ -13,3 +15,32 @@ person1 = Person("neha",19)
 
 #calling methods
 person1.greet()
+
+
+#encapsulation
+
+#class constructor
+class BankAccount:
+    def __init__(self,AcNo,Bal):
+        self.AcNo=AcNo
+        self.Bal=Bal
+
+    def deposit(self,amount):  
+        self.Bal += amount
+
+    def withdraw(self,amount):
+        if self.Bal >= amount:
+            self.Bal -= amount
+        else:
+            print("Insufficient funds to withdraw")
+
+    def getBalance(self):
+        print(self.Bal)
+
+#creating objects instance
+account=BankAccount("1234",2000) 
+
+#calling methods
+account.deposit(500)
+account.withdraw(300)
+account.getBalance()
