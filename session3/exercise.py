b8ce8287c0c52a2d23c4c132e3bd15f9110a199a
NehaS101 <solankineha297@gmail.com>
2023-07-08 22:25:51 +0530

    problems updated

---
+++ session3/exercise.py
@@ -0,0 +1,12 @@
+'''
+Exercise 1:
+Write a program that finds the largest element in a list. 
+Prompt the user to enter a list of numbers, and then display the largest number.
+'''
+
+
+'''
+Exercise 2:
+Write a program that counts the frequency of words in a given sentence. 
+Prompt the user to enter a sentence, and then display the frequency of each word.
+'''
---
+++ session4/exercise.py
@@ -0,0 +1,11 @@
+'''
+Exercise 1:
+Write a program that reads data from a file called "numbers.txt" and 
+calculates the average, maximum, and minimum values from the numbers in the file.
+'''
+
+'''
+Exercise 2:
+Modify the program from Exercise 1 to handle file-related errors. 
+If the file is not found, display an error message.
+'''
