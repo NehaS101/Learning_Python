caba1b228dd39200dbe13933cf65feb8373447fe
NehaS101 <solankineha297@gmail.com>
2023-07-09 12:41:41 +0530

    problem update

---
+++ session6/exercise.py
@@ -0,0 +1,9 @@
+'''
+Exercise 1:
+Write a function that accepts two integers as arguments and divides the first number by the second number. 
+Handle any potential exceptions that may occur, such as division by zero or invalid input.
+
+Exercise 2:
+Write a program that prompts the user to enter a file name. Open the file and display its content. 
+If the file is not found, handle the exception and display an appropriate error message.
+'''
